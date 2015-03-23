02d2d094a915b4e41e781e287f9cd75e3258ecf0
Diego Pinheiro <diegoquintanap@gmail.com>
2015-03-23 18:03:13 -0400

    Improve cfg.py comments

--- cfg/cfg.py
+++ cfg/cfg.py
@@ -6,35 +6,50 @@ from . import cfg_ast_visitor, cfg_wcec
 
 
 class CFG(object):
-    """ CFG is made of a list of entry nodes that represents
-        all functions defined in the source code. Each entry
-        node has the function name and its start node. Given
-        the start node, everyone can be achieved.
+    """ CFG is made of a list of entry nodes that represents all functions
+        defined in the source code. Each entry node has the function name and
+        its start node. Given the start node, everyone can be achieved.
+        Moreover, this classs also keeps its AST.
 
-        All visit functions must always return an CFGNode.
-        However, when a function definitions is being visited,
-        an CFGEntryNode should be returned.
+        Args:
+            filename (string): C file name
 
-        PS1: It is helpful to look to pycparser/_c_ast.cfg at
-        the same time, because of nodes structures.
-
-        PS2: This code was strictly based on pycparser/c_ast.py.
-        This class is not a subclass of c_ast.NodeVisitor,
-        because generic_visit() should have some changes.
+        Attributes:
+            filename (string): C file name
+            ast (pycparser/c_ast): Abstract Syntax Tree
+            entry_nodes (CFGEntryNode): list of all functions presented in AST
     """
 
     def __init__(self, filename):
+        """ Initialize attributes
+
+            Args:
+                filename (string): C file name
+        """
         self._filename = filename
         self._ast = None
         self._entry_nodes = []
 
     def get_entry_nodes(self):
+        """ Returns:
+                list of all functions parsed by the AST
+        """
         return self._entry_nodes
 
     def get_ast(self):
+        """ Returns:
+                Abstract syntax tree as pycparser/c_ast object
+        """
         return self._ast
 
     def make_cfg(self):
+        """ Parser the source code using pycparser to generate the AST, then
+            explore the AST to generate the CFG. After that, WCEC and RWCEC are
+            computed.
+
+            Returns:
+                list of all functions parsed by the AST
+        """
         # run pycparser
         self._ast = parse_file(self._filename, use_cpp=True,
                                 cpp_path='gcc',
@@ -42,12 +57,22 @@ class CFG(object):
         # explore AST and make CFG
         ast_visitor = cfg_ast_visitor.CFGAstVisitor()
         self._entry_nodes = ast_visitor.make_cfg_from_ast(self._ast)
-        self._compute_wcec()
+        self._compute_wcec_rwcec()
+        return self._entry_nodes
 
-    def _compute_wcec(self):
+    def _compute_wcec_rwcec(self):
+        """ Compute WCEC and RWCEC of the given CFG.
+        """
         wcec = cfg_wcec.CFGWCEC(self._filename, self)
         wcec.compute_cfg_wcec()
 
     def show(self, buf=sys.stdout):
+        """ Display in standard output if no parameter is given all CFG's
+            nodes, each one children and their start line,
+
+            Args:
+                buf (file): file object to write CFG. If no file is provided,
+                    then writes in standard output.
+        """
         for entry_point in self.get_entry_nodes():
             entry_point.show(buf=buf)
